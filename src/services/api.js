0f3d606c8baffb042a571c648bb120a6efb13f9e
IsaiahIso <lesorogol1@gmail.com>
2025-05-10 19:16:01 +0300

    My Movie Appo

---
+++ src/services/api.js
@@ -0,0 +1,21 @@
+const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+
+if (!API_KEY) {
+  console.error("API key is missing!");
+}
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    );
+    const data = await response.json();
+    return data.results;
+};
